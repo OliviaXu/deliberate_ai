0e8227378dcdb621971f737749b42f2b32a05a74
Olivia Xu <koala@redwood.local>
2026-03-07 06:42:55 -0500

    Refactor CSS to use Tailwind layers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{ts,tsx,css}', './entrypoints/**/*.{ts,tsx,html}'],
   darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        deliberate: ['"Google Sans Text"', '"Google Sans"', 'Sohne', 'Inter', '"Segoe UI"', 'system-ui', 'sans-serif'],
+        journal: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    }
+  },
   corePlugins: {
     preflight: false
   }
